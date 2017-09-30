4a6ed6cbb2d54db54bbe9e92c9a0c3bad019c90a
Addison Webb <addisonwebb@icloud.com>
2017-09-30 14:15:51 -0500

    update package for Swift 4.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,24 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "KituraApiToken",
+    products: [
+        .library(
+            name: "KituraApiToken",
+            targets: ["KituraApiToken"])
+    ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7)
+        .package(
+            url: "https://github.com/IBM-Swift/Kitura.git", from: "1.7.9")
+    ],
+    targets: [
+        .target(
+            name: "KituraApiToken",
+            dependencies: ["Kitura"],
+            // need to define path for now
+            // https://bugs.swift.org/browse/SR-5799?page=com.atlassian.jira.plugin.system.issuetabpanels%3Achangehistory-tabpanel
+            path: "Sources")
     ]
 )
